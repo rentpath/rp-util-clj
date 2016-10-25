111180ae89e8b9cf3f8648b5a0a1ff3d57b9e808
Scott McLeod <halcyonblue@gmail.com>
2016-10-24 21:24:01 -0400

    [SM][000000] Catch exceptions thrown from parsing octal strings

--- src/rp/util/string.clj
+++ src/rp/util/string.clj
@@ -2,9 +2,11 @@
   (:require [clojure.tools.reader.edn :as edn]
             [clojure.string :as str]))
 
-(def long-regex (re-pattern "([-+]?)(?:(0)|([1-9][0-9]*)|0[xX]([0-9A-Fa-f]+)|0([0-7]+)|([1-9][0-9]?)[rR]([0-9A-Za-z]+)|0[0-9]+)(N)?"))
-(def double-regex (re-pattern "([-+]?[0-9]+(\\.[0-9]*)?([eE][-+]?[0-9]+)?)(M)?"))
-(def bool-regex (re-pattern "(?i)true|false"))
+(def ^:const long-regex (re-pattern "([-+]?)(?:(0)|([1-9][0-9]*)|0[xX]([0-9A-Fa-f]+)|0([0-7]+)|([1-9][0-9]?)[rR]([0-9A-Za-z]+)|0[0-9]+)(N)?"))
+(def ^:const double-regex (re-pattern "([-+]?[0-9]+(\\.[0-9]*)?([eE][-+]?[0-9]+)?)(M)?"))
+(def ^:const bool-regex (re-pattern "(?i)true|false"))
+
+(def ^:const comma-regex #",")
 
 (defn parseable?
   [re s]
@@ -14,13 +16,16 @@
   [regex]
   (fn [s]
     (when (and (string? s) (parseable? regex s))
-      (edn/read-string (str/lower-case s)))))
+      (try
+        (edn/read-string s)
+        (catch Exception e)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Public API
 
 (def parse-long (make-parser long-regex))
-(def parse-boolean (make-parser bool-regex))
+(def parse-boolean (comp (make-parser bool-regex)
+                         #(when % (str/lower-case %))))
 (def parse-double* (make-parser double-regex))
 
 (defn parse-double
@@ -28,6 +33,12 @@
   (some-> s
           parse-double*
           double))
+
+(defn parse-big-decimal
+  [s]
+  (some-> s
+          parse-double*
+          bigdec))
 
 (defn non-blank-string
   "Returns x if it is a non-blank string, nil otherwise."
