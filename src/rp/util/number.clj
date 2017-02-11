631ef5f67ed51b4d5023c4c6659d49104dff85ae
Scott McLeod <halcyonblue@gmail.com>
2017-02-11 00:47:48 -0500

    Add bounding-box and refactor conformers for readability.

--- src/rp/util/number.clj
+++ src/rp/util/number.clj
@@ -4,3 +4,13 @@
   [x]
   (and (number? x)
        (not (neg? x))))
+
+(defn longitude?
+  [x]
+  (and (number? x)
+       (<= -180 x 180)))
+
+(defn latitude?
+  [x]
+  (and (number? x)
+       (<= -90 x 90)))
