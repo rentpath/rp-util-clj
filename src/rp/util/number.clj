db29f53235e1ae4acbc0007ea81d13a3c0ec2e37
Scott McLeod <halcyonblue@gmail.com>
2017-02-13 13:22:50 -0500

    Add predicate for checking if coordinates are within a bounding box

--- src/rp/util/number.clj
+++ src/rp/util/number.clj
@@ -14,3 +14,8 @@
   [x]
   (and (number? x)
        (<= -90 x 90)))
+
+(defn within-bounding-box?
+  [[lng1 lat1 lng2 lat2] [longitude latitude]]
+  (and (<= lng1 longitude lng2)
+       (<= lat1 latitude lat2)))
