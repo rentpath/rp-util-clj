634c77ec6f3d1fd62b966e177bf7690a35f58a2d
Jenkins Continous Integration <jenkins@jenkins-slave-02.ci.atl.primedia.com>
2017-06-29 14:57:24 -0400

    Version 1.0.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rentpath/rp-util-clj "1.0.9-SNAPSHOT"
+(defproject com.rentpath/rp-util-clj "1.0.9"
   :description "Common utilities"
   :url "https://github.com/rentpath/rp-util-clj"
   :license {:name "Eclipse Public License"
