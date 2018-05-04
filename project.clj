67aa2bcd89bc47b136e4c076f2ca97f7b8b67495
Jenkins Service <jenkins@jenkins-slave005.tools.useast2.rentpath.com>
2018-05-04 11:04:59 -0400

    Version 1.0.16-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rentpath/rp-util-clj "1.0.15"
+(defproject com.rentpath/rp-util-clj "1.0.16-SNAPSHOT"
   :description "Common utilities"
   :url "https://github.com/rentpath/rp-util-clj"
   :license {:name "Eclipse Public License"
